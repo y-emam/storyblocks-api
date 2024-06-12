7ac8e992ac0f707f33d4420d1661a31103da83d0
y-emam <YasserEmam2002@gmail.com>
2024-06-13 01:05:36 +0300

    working on the renaming structure

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import fs from "fs";
 import getLinks from "../../services/getLinks";
 import "./Home.css";
 
@@ -15,9 +17,37 @@ const Home = () => {
   const downloadAllVideos = async () => {
     const hiddenLinks = document.getElementById("hidden-links").children;
 
-    for (let i = 0; i < hiddenLinks.length; i++) {
-      window.open(hiddenLinks[i].href);
-    }
+    axios({
+      url: hiddenLinks[0].href,
+      method: "GET",
+      responseType: "stream", // Important: responseType must be 'stream' to handle binary files
+    })
+      .then((response) => {
+        // Create a writable stream and pipe the response data to it
+        const writer = fs.createWriteStream("video.mp4");
+        response.data.pipe(writer);
+
+        // Handle successful response
+        writer.on("finish", () => {
+          console.log("Video downloaded successfully");
+
+          fs.rename("nature.mp4", "D:/Media/pexels/nature.mp4", (err) => {
+            if (err) {
+              console.error("Error moving file", err);
+            } else {
+              console.log("File saved to destination");
+            }
+          });
+        });
+
+        // Handle errors
+        writer.on("error", (err) => {
+          console.error("Error downloading video", err);
+        });
+      })
+      .catch((err) => {
+        console.error("Error downloading video", err);
+      });
   };
 
   return (
@@ -34,11 +64,11 @@ const Home = () => {
         </label>
         <input type="submit" />
       </form>
-      <ul id="links-textarea"></ul>
-      <div id="hidden-links" hidden={true}></div>
       <button className="download-all" onClick={downloadAllVideos}>
         Download All Links
       </button>
+      <ul id="links-textarea"></ul>
+      <div id="hidden-links" hidden={true}></div>
     </div>
   );
 };
--- src/services/getLinks.js
+++ src/services/getLinks.js
@@ -1,7 +1,7 @@
 import preprocessScript from "./preprocessScript";
 import requestPexelsApi from "./requestPexelsApi";
 
-const getLinks = async (script, quality = "_720p", noVideos = 3) => {
+const getLinks = async (script, quality = "_720p", noVideos = 5) => {
   const keywords = preprocessScript(script);
 
   for (let i = 0; i < keywords.length; i++) {
